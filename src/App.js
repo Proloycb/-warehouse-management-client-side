8c87a57041ad72dc9f5056bf497ca20c0a0baade
Proloycb <proloychacroborty@gmail.com>
2022-05-08 14:18:13 +0600

    footer part completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home/Home';
 import Header from './pages/Shared/Header/Header';
+import Footer from './pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            
-        </div>
+       <footer className='text-center bg-secondary mt-5 p-3'>
+           <p className='text-light'><small>Copyright &copy; {year}</small></p>
+       </footer>
     );
 };
 
