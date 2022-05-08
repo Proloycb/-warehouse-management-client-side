310247350852d0ea334618ccdd79081b637e6baa
Proloycb <proloychacroborty@gmail.com>
2022-05-08 20:43:14 +0600

    inventories item button clicked complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home/Home';
 import Header from './pages/Shared/Header/Header';
 import Footer from './pages/Shared/Footer/Footer';
+import InventoryDetail from './pages/InventoryDetail/InventoryDetail';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/inventory/:id' element={<InventoryDetail/>}/>
       </Routes>
       <Footer/>
     </div>
--- src/pages/InventoryDetail/InventoryDetail.js
+++ src/pages/InventoryDetail/InventoryDetail.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import useInventoryDetail from '../../hooks/useInventoryDetail';
 
-const Inventory = ({ inventory }) => {
-    const { _id, name, image, description, price, quantity, supplier } = inventory;
-    const navigate = useNavigate();
+const InventoryDetail = () => {
+    const {id} = useParams();
+    const [inventory] = useInventoryDetail(id);
+    const {image, name, price, quantity, supplier, description} = inventory;
 
-    const handleUpdate = id => {
-        navigate(`/inventory/${id}`);
-    }
     return (
-        <div className="card-group col">
-
+        <div style={{width: '32rem'}} className='mx-auto mt-3'>
             <div className="card">
-                <img src={image} className="card-img-top" alt="..." />
+                <img style={{width: '22rem'}} src={image} className="card-img-top mx-auto" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text mb-1">Price: ${price}</p>
@@ -21,10 +18,10 @@ const Inventory = ({ inventory }) => {
                     <p className="card-text mb-1">Supplier:<small> {supplier}</small></p>
                     <p className="card-text"><small className="text-muted">{description}</small></p>
                 </div>
-                <Button onClick={() => handleUpdate(_id)} variant='success'>Update</Button>
+                {/* <Button onClick={() => handleUpdate(_id)} variant='success'>Update</Button> */}
             </div>
         </div>
     );
 };
 
-export default Inventory;
+export default InventoryDetail;
